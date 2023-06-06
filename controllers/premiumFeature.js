a7b3e56d47419470eb678b9abffc778402cf3a30
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-06-06 21:35:42 +0530

    downloadfeature

--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -1,5 +1,6 @@
 const User = require('../models/users');
-const Expense = require('../models/expense');
+const AWS = require('aws-sdk');
+require('dotenv').config()
 
 exports.getLeaderboard = async(req, res, next)=>{
     try{
@@ -11,3 +12,45 @@ exports.getLeaderboard = async(req, res, next)=>{
         res.status(500).json(err);
     }
 }
+
+exports.downloadfile = async (req, res, next)=>{
+    try{
+    const expenses = await req.user.getExpenses();
+    const stringifyexpenses = JSON.stringify(expenses);
+    const userid = req.user.id;
+    const filename = `Expense${userid}/${new Date()}.txt`;
+    const fileUrl = await uploadToS3(stringifyexpenses, filename);
+    res.status(200).json(fileUrl);
+    } 
+    catch(err){
+        console.log(err);
+        res.status(400).json('something went wrong', err);
+    }
+}
+
+async function uploadToS3(data, filename){
+    const bucketName = process.env.BUCKET_NAME;
+    const IAMuserkey = process.env.IAM_USER_KEY;
+    const IAMusersecret = process.env.IAM_USER_SECRET;
+
+    let s3bucket = new AWS.S3({
+        accessKeyId : IAMuserkey,
+        secretAccessKey: IAMusersecret
+    })
+        var params= {
+            Bucket: bucketName,
+            Key: filename,
+            Body: data,
+            ACL: 'public-read'
+        }
+        return new Promise((resolve, reject)=>{
+            s3bucket.upload(params, (err,s3response)=>{
+            if(err){
+                console.log('something went wrong', err);
+            }
+            else{
+                console.log('success', s3response);
+                resolve(s3response.Location);
+            }
+        })})
+    }
--- routers/premiumFeature.js
+++ routers/premiumFeature.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const PremiumFeatureController = require('../controllers/premiumFeature');
+const UserController = require('../controllers/users');
 
 router.get('/leaderboard', PremiumFeatureController.getLeaderboard);
+
+router.get('/download', UserController.decodeToken, PremiumFeatureController.downloadfile);
 
 module.exports = router;
